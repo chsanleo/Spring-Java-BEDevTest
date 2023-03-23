8c24abd832d76cbe2e35e478e6f5844187f67988
Christian <chsanleo@gmail.com>
2023-03-24 00:51:05 +0100

    Change to list

--- src/main/java/com/myapp/restapi/client/ProductInfoAPI.java
+++ src/main/java/com/myapp/restapi/client/ProductInfoAPI.java
@@ -1,9 +1,11 @@
 package com.myapp.restapi.client;
 
 import com.myapp.restapi.model.ProductDetail;
-import com.myapp.restapi.model.SimilarProducts;
+
+import java.util.List;
 
 public interface ProductInfoAPI {
-    SimilarProducts getSimilarProduct(String productId);
+    List<String> getSimilarProduct(String productId);
+
     ProductDetail getProduct(String productId);
 }
--- src/main/java/com/myapp/restapi/client/impl/ProductInfoAPIImpl.java
+++ src/main/java/com/myapp/restapi/client/impl/ProductInfoAPIImpl.java
@@ -2,12 +2,12 @@ package com.myapp.restapi.client.impl;
 
 import com.myapp.restapi.client.ProductInfoAPI;
 import com.myapp.restapi.model.ProductDetail;
-import com.myapp.restapi.model.SimilarProducts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class ProductInfoAPIImpl implements ProductInfoAPI {
@@ -16,16 +16,14 @@ public class ProductInfoAPIImpl implements ProductInfoAPI {
     private String productsInfoApi;
 
     @Override
-    public SimilarProducts getSimilarProduct(String productId) {
+    public List<String> getSimilarProduct(String productId) {
 
         var productsApi = productsInfoApi + "/product/" + productId + "/similarids";
 
         var restTemplate = new RestTemplate();
         var response = restTemplate.getForObject(productsApi, String[].class);
 
-        var similarProducts = new SimilarProducts();
-        similarProducts.setProductIds(Arrays.asList(response));
-        return similarProducts;
+        return Arrays.asList(response);
     }
 
     @Override
