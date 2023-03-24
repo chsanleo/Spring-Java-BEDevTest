958c279c723d62b219214b44606ea73e653812ee
Christian <chsanleo@gmail.com>
2023-03-24 01:08:13 +0100

    add validation of variable and throw exception

--- src/main/java/com/myapp/restapi/service/impl/ProductServiceImpl.java
+++ src/main/java/com/myapp/restapi/service/impl/ProductServiceImpl.java
@@ -3,8 +3,11 @@ package com.myapp.restapi.service.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myapp.restapi.client.ProductInfoAPI;
 import com.myapp.restapi.dto.ProductDetailDTO;
+import com.myapp.restapi.exceptions.FormatNotValidException;
 import com.myapp.restapi.exceptions.ProductNotFoundException;
 import com.myapp.restapi.service.ProductService;
+import io.micrometer.common.util.StringUtils;
+import io.netty.util.internal.StringUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -22,7 +25,10 @@ public class ProductServiceImpl implements ProductService {
     private ObjectMapper objectMapper;
 
     @Override
-    public List<ProductDetailDTO> similarTo(String productId) throws ProductNotFoundException {
+    public List<ProductDetailDTO> similarTo(String productId) throws ProductNotFoundException, FormatNotValidException {
+
+        validateProductId(productId);
+
         var similarProducts = api.getSimilarProduct(productId);
 
         if (CollectionUtils.isEmpty(api.getSimilarProduct(productId))){
@@ -34,5 +40,11 @@ public class ProductServiceImpl implements ProductService {
                 .filter(Objects::nonNull)
                 .map(productDetail -> objectMapper.convertValue(productDetail, ProductDetailDTO.class))
                 .collect(Collectors.toList());
+    }
+
+    private void validateProductId(String productId) throws FormatNotValidException {
+        if(StringUtils.isBlank(productId)){
+            throw new FormatNotValidException("Formar not valid of: "+ productId);
+        }
     }
 }
