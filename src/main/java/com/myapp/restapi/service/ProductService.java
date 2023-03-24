1457aa7b04921e32c268b59670d41c50264ee15d
Christian <chsanleo@gmail.com>
2023-03-24 01:08:23 +0100

    add throw exception

--- src/main/java/com/myapp/restapi/service/ProductService.java
+++ src/main/java/com/myapp/restapi/service/ProductService.java
@@ -1,11 +1,12 @@
 package com.myapp.restapi.service;
 
 import com.myapp.restapi.dto.ProductDetailDTO;
+import com.myapp.restapi.exceptions.FormatNotValidException;
 import com.myapp.restapi.exceptions.ProductNotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
 
-    List<ProductDetailDTO> similarTo(String productId) throws ProductNotFoundException;
+    List<ProductDetailDTO> similarTo(String productId) throws ProductNotFoundException, FormatNotValidException;
 }
